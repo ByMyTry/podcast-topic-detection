51694407094a6c46d2215e29b156d6bcabb8e1a8
Anton Taleckij <anton.taleckij@gmail.com>
2019-04-13 15:11:06 +0300

    implemented full file analysis

--- main.py
+++ main.py
@@ -11,12 +11,17 @@ DATA_DIR = 'data'
 PODCAST_FILE_NAME_PREFIX = 'podcast'
 PODCAST_FILE_FORMAT = 'wav'
 TEMP_FILE_NAME_PREFIX = 'tmp'
-NUM_TOPICS = 2
-NUM_WORDS = 5
+STEP = 100
+ANALIZED_PERCENT = 1.0  # 0.6
+NUM_TOPICS = 10
+NUM_WORDS = 3
 
 
 def main():
-    podcast_file_name = download_podcast(url='https://www.youtube.com/watch?v=J5O5iRpLXKY&t=637s')
+    podcast_file_name = download_podcast(
+        url='https://www.youtube.com/watch?v=in7tepc2shg'
+        # url='https://www.youtube.com/watch?v=J5O5iRpLXKY&t=637s'
+    )
     text = recognize_text(podcast_file_name)
     tokens = prepare_text_for_lda(text)
     topics = get_topics_by_lda(tokens)
@@ -30,20 +35,20 @@ def _get_file_name(file_name_prefix, file_format):
 def download_podcast(url=None):
     if url is not None:
         podcast_file_name = _get_file_name(PODCAST_FILE_NAME_PREFIX, PODCAST_FILE_FORMAT)
-        # ydl_opts = {
-        #     'format': 'bestaudio/best',
-        #     'postprocessors': [{
-        #         'key': 'FFmpegExtractAudio',
-        #         'preferredcodec': PODCAST_FILE_FORMAT,
-        #         'preferredquality': '192'
-        #     }],
-        #     'outtmpl': PODCAST_FILE_NAME_PREFIX,
-        #     'prefer_ffmpeg': True,
-        #     'keepvideo': False
-        # }
-        # with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        #     ydl.download([url])
-        # os.rename(PODCAST_FILE_FORMAT, os.path.join(DATA_DIR, podcast_file_name))
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': PODCAST_FILE_FORMAT,
+                'preferredquality': '192'
+            }],
+            'outtmpl': PODCAST_FILE_NAME_PREFIX,
+            'prefer_ffmpeg': True,
+            'keepvideo': False
+        }
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+        os.rename(PODCAST_FILE_FORMAT, os.path.join(DATA_DIR, podcast_file_name))
         return podcast_file_name
     else:
         raise Exception('url is empty')
@@ -54,17 +59,22 @@ def recognize_text(podcast_file_name):
     tmp_file_name = _get_file_name(TEMP_FILE_NAME_PREFIX, PODCAST_FILE_FORMAT)
 
     # AudioSegment.converter = which("ffmpeg")
-    for i in [1]:
-        t1 = 110 * 1000  # Works in milliseconds
-        t2 = 200 * 1000
-        new_audio = AudioSegment.from_wav(os.path.join(DATA_DIR, podcast_file_name))
-        new_audio = new_audio[t1:t2]
-        new_audio.export(os.path.join(DATA_DIR, tmp_file_name), format=PODCAST_FILE_FORMAT)
+    audio = AudioSegment.from_wav(os.path.join(DATA_DIR, podcast_file_name))
+    fragments_count = int(len(audio) / (STEP * 1000) * ANALIZED_PERCENT)
+    for i in range(fragments_count):
+        print(i, '/', fragments_count)
+        try:
+            t1 = i * STEP * 1000  # Works in milliseconds
+            t2 = (i+1) * STEP * 1000
+            new_audio = audio[t1:t2]
+            new_audio.export(os.path.join(DATA_DIR, tmp_file_name), format=PODCAST_FILE_FORMAT)
 
-        recognizer = sr.Recognizer()
-        with sr.AudioFile(os.path.join(DATA_DIR, tmp_file_name)) as source:
-            podcast_audio = recognizer.record(source)
-        text_chunks.append(recognizer.recognize_google(podcast_audio, language='ru_RU').lower())
+            recognizer = sr.Recognizer()
+            with sr.AudioFile(os.path.join(DATA_DIR, tmp_file_name)) as source:
+                podcast_audio = recognizer.record(source)
+            text_chunks.append(recognizer.recognize_google(podcast_audio, language='ru_RU').lower())
+        except Exception as e:
+            print(e)
     return ' '.join(text_chunks)
 
 
